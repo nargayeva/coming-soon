fc74d1c78c4054e3190d9c703af3730b61607903
Narmina Aghayeva <nargayeva@gmail.com>
2021-05-03 10:21:16 +0400

    slideshow added

---
+++ script.js
@@ -0,0 +1,22 @@
+mibreitGallery.createGallery({
+
+    // slideshow container
+    slideshowContainer: "#full-gallery",
+  
+    // thumb view container
+    thumbviewContainer: ".mibreit-thumbview",
+  
+    // image title container
+    titleContainer: "#full-gallery-title",
+  
+    // determine whether to enable fullscreen mode
+    allowFullscreen: !0,
+  
+    // image preloader
+    preloadLeftNr: 2,
+    preloadRightNr: 3,
+  
+    // SCALE_MODE_STRETCH, SCALE_MODE_FITASPECT (defualt), SCALE_MODE_NONE, SCALE_MODE_EXPAND
+    scaleMode: SCALE_MODE_FITASPECT
+  
+  })
